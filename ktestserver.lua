a750fa57c1ec8f61b4f38945c84fef187a91e313
TacticalSlime <robloxnoob64@gmail.com>
2025-11-03 09:59:04 -0800

    Lots of fixes and additions to a functional state

--- ktestserver.lua
+++ ktestserver.lua
@@ -1,11 +1,16 @@
 local kserver = dofile("lib/kserver.lua")
+kserver.open()
 
 print(os.getComputerID())
 
-local testFunc = kserver.servFunc.new()
-testFunc.name = "testFunc"
-testFunc.func = function() return "Hewwo!" end
-
 local server = kserver.server.new()
-server.funcList = funcs
+server.debug = true
+server.funcList.testFunc = kserver.servFunc.new()
+server.funcList.testFunc.name = "testFunc"
+server.funcList.testFunc.func = function() print("Weh!") return "weh!" end
+
+for i,v in pairs(server.funcList.testFunc) do
+    print(i..": "..tostring(v))
+end
+
 server:run("kserver")
